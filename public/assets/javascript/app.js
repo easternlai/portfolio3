245e36fcaec1405c57f8612c6607c6eb65422263
Eastern Lai <easternlai@hotmail.com>
2020-02-04 22:46:59 -0800

    added testimonial

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,15 +1,47 @@
-$(".about-but").on("click",function(event){
-    $(".portfolio").addClass("hidden");
-    $(".about-but").addClass("font-change");
-    $(".about-me").removeClass("hidden");
-    $(".portfolio-but").removeClass("font-change");
-});
+$(document).ready(function() {
 
-$(".portfolio-but").on("click",function(event){
-    $(".portfolio").removeClass("hidden");
-    $(".about-but").removeClass("font-change");
-    $(".portfolio-but").addClass("font-change");
-    $(".about-me").addClass("hidden");
-});
+    $(".about-but").on("click",function(event){
+        $(".about-me").removeClass("hidden");
+        $(".portfolio-but").removeClass("font-change");
+        $(".testimonial-but").removeClass("font-change");
+        $(".about-but").addClass("font-change");
+        $(".portfolio").addClass("hidden");
+        $(".testimonial").addClass("hidden");
 
+
+    });
+
+    $(".portfolio-but").on("click",function(event){
+        $(".portfolio").removeClass("hidden");
+        $(".about-but").removeClass("font-change");
+        $(".testimonial-but").removeClass("font-change");
+        $(".portfolio-but").addClass("font-change");
+        $(".about-me").addClass("hidden");
+        $(".testimonial").addClass("hidden");
+    });
+
+    $(".testimonial-but").on("click",function(event){
+        $(".testimonial").removeClass("hidden");
+        $(".portfolio-but").removeClass("font-change");
+        $(".about-but").removeClass("font-change");
+        $(".testimonial-but").addClass("font-change");
+        $(".about-me").addClass("hidden");
+        $(".portfolio").addClass("hidden");
+    });
+
+    $(".testimonial-submit").on("submit", handleSubmit);
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        var newTestimonial = {
+            firstName = $("#name-input").val().trim(),
+            body = $("#testimonial-input").val().trim()
+        };
+        $.post("/api/testimonial", newTestimonial, function(){
+            alert("Thank you!");
+        });
+    }
+
+});
 
